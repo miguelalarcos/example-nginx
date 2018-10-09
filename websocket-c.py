213626aa2557bda8bee29b77a742d1bb86fef0c1
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2018-10-09 15:54:04 +0200

    websocket with ssl working

--- websocket-c.py
+++ websocket-c.py
@@ -1,8 +1,14 @@
 import asyncio
 import websockets
+import pathlib
+import ssl
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.load_verify_locations(
+    pathlib.Path(__file__).with_name('host.pem'))
 
 async def hello():
-    async with websockets.connect('ws://localhost:9001') as websocket:
+    async with websockets.connect('wss://localhost:9001', ssl=ssl_context) as websocket:
         name = input("What's your name? ")
 
         await websocket.send(name)
